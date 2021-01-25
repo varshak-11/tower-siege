a9f7b61a901864f13f32d23b4d0684d7963b401a
varshak-11 <72525719+varshak-11@users.noreply.github.com>
2021-01-25 16:59:12 -0500

    Add files via upload

---
+++ polygon.js
@@ -0,0 +1,13 @@
+class Polygon extends BaseClass {
+    constructor(x,y){
+      super(x,y,50,50);
+      this.image = loadImage("polygon.png");
+    }
+  
+    display() {
+      //this.body.position.x = mouseX;
+      //this.body.position.y = mouseY;
+      super.display();
+    }
+  }
+  
